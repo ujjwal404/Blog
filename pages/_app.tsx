af09060436a2011d7cddd425fb888fbc625962ee
Ujjwal <ujjwal2022.cs19@nsut.ac.in>
2023-01-13 16:27:27 +0530

    added more meta tags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,11 +3,17 @@ import '../styles/globals.css';
 import { ThemeProvider } from 'next-themes';
 import Layout from '../components/layout';
 import '../styles/github-dark.css'
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
 		<ThemeProvider attribute="class">
-		<Layout>
+			<Layout>
+				 <Head>
+        <meta name="keywords" content="blogs ujjwal blogsite" />
+        <meta name="author" content="Ujjwal" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
 			<Component {...pageProps} />
 		</Layout>
 		</ThemeProvider>
