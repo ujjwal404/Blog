42e47d486f143c74e514f6d9db6bb0f2aa60324f
Ujjwal <ujjwal2022.cs19@nsut.ac.in>
2023-01-22 11:43:21 +0530

    domain updated

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
 	return (
 		<footer className="dark:bg-darkGray bg-gray h-10 md:h-16 mt-0 md:mt-10">
 			<div className="md:max-w-5xl mx-auto">
-			<h1 className="mx-auto mt-4 md:mt-6 text-xs md:text-md ml-3 md:ml-0">© 2022 ujjwalkadam.tech</h1>
+			<h1 className="mx-auto mt-4 md:mt-6 text-xs md:text-md ml-3 md:ml-0">© 2022 ujjwalkadam.me</h1>
 			</div>
 		</footer>
 	);
